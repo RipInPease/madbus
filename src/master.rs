60eef810f8e3fa627997aa09dbaa624abcce3298
RipInPease <svorrevfjelde@gmail.com>
2026-01-11 16:32:51 +0100

    Forgot to save

--- src/master.rs
+++ src/master.rs
@@ -2,22 +2,6 @@ use std::io::{Read, Write, Result as IOResult};
 use crate::serial_port::SerialPort;
 
 
-macro_rules! new_function {
-    ( $t:ty, $( $field:ident : $ty:ty ),* ) => {
-
-        impl $t {
-            /// Creates a new instance of this type with given inputs
-            pub fn new( $( $field: $ty ),* ) -> Self {
-                Self {
-                    $( $field ),*
-                }
-            }
-        }
-
-    };
-}
-
-
 /// The master in the modbus line
 /// 
 pub struct Master {
