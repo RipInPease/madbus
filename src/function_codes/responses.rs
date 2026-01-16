19ff83bc8186fc0383167f3b59f242c80ad74ef2
RipInPease <svorrevfjelde@gmail.com>
2026-01-16 20:54:03 +0100

    ResponseData and Response

--- src/function_codes/responses.rs
+++ src/function_codes/responses.rs
@@ -2,6 +2,100 @@ use std::io::Read;
 use crate::ReadGet;
 
 
+pub struct Response {
+    /// The slave address who sent the response
+    addr: u8,
+
+    /// The response itself
+    data: ResponseData
+}
+
+
+impl ReadGet for Response {
+    fn read_get(reader: &mut impl Read) -> Option<Self> 
+    where Self: Sized 
+    {
+        let mut bfr = [0];
+        match reader.read(&mut bfr) {
+            Ok(count)   => if count < 1 { return None },
+            Err(_)      => return None
+        }
+
+        let addr = bfr[0];
+        let data = ResponseData::read_get(reader)?;
+
+        Some(Self{ addr, data })
+  
+    }
+}
+
+
+impl Into<Vec<u8>> for Response {
+    fn into(self) -> Vec<u8> {
+        let mut v = vec![self.addr];
+
+        let data: Vec<u8> = self.data.into();
+        v.extend_from_slice(&data);
+
+        v
+    }
+}
+
+
+pub enum ResponseData {
+    ReadCoils(ReadCoilsResp),
+    ReadDI(ReadDIResp),
+    ReadHolding(ReadHoldingResp),
+    ReadInput(ReadInputResp),
+}
+
+
+impl ReadGet for ResponseData {
+    fn read_get(reader: &mut impl Read) -> Option<Self> 
+    where Self: Sized 
+    {
+        let mut bfr = [0];
+        match reader.read(&mut bfr) {
+            Ok(count)   => if count < 1 { return None },
+            Err(_)      => return None
+        }
+
+        match bfr[0] {
+            1 => Some(Self::ReadCoils(ReadCoilsResp::read_get(reader)?)),
+            2 => Some(Self::ReadDI(ReadDIResp::read_get(reader)?)),
+            3 => Some(Self::ReadHolding(ReadHoldingResp::read_get(reader)?)),
+            4 => Some(Self::ReadInput(ReadInputResp::read_get(reader)?)),
+            _ => None
+        }
+    }
+}
+
+
+impl Into<Vec<u8>> for ResponseData {
+    fn into(self) -> Vec<u8> {
+        let mut v = Vec::new();
+
+        match self {
+            Self::ReadCoils(_)     => v.push(1),
+            Self::ReadDI(_)    => v.push(2),
+            Self::ReadHolding(_)   => v.push(3),
+            Self::ReadInput(_)     => v.push(4),
+        };
+
+        let bytes: Vec<u8> = match self {
+            Self::ReadCoils(data)     => data.into(),
+            Self::ReadDI(data)    => data.into(),
+            Self::ReadHolding(data)   => data.into(),
+            Self::ReadInput(data)     => data.into(),
+        };
+
+        v.extend_from_slice(&bytes);
+
+        v
+    }
+}
+
+
 /// The response for ReadCoil command
 /// 
 #[derive(Clone, Debug)]
@@ -110,6 +204,38 @@ impl ReadDIResp {
 
 
         Self{ bytes, coils: v }
+    }
+}
+
+
+impl ReadGet for ReadDIResp {
+    fn read_get(reader: &mut impl Read) -> Option<Self> 
+    where Self: Sized 
+    {
+        let mut bfr = [0];
+        match reader.read(&mut bfr) {
+            Ok(count)   => if count < 1 { return None },
+            Err(_)      => return None
+        }
+
+        let bytes = bfr[0];
+        if bytes % 2 != 0 { return None }
+
+        let mut bfr = vec![0; bytes as usize];
+        if reader.read_exact(&mut bfr).is_err() {
+            return None
+        }
+
+        let mut coils: Vec<bool> = Vec::with_capacity(bytes as usize / 2);
+
+        for mut bytes in bfr {
+            for _ in 0..8 {
+                coils.push(bytes & 1 == 1);
+                bytes >>= 1;
+            }
+        }
+
+        Some(Self{ bytes, coils })
     }
 }
 
