68d6e07f37153279b76b8d13e889ed81042f2970
Snorre Vandbakk Fjelde <snorre.vandbakk.fjelde@oneco.no>
2026-02-04 08:56:37 +0100

    Impl Exception

--- src/exception_codes.rs
+++ src/exception_codes.rs
@@ -43,3 +43,36 @@ impl Exception {
         }
     }
 }
+
+
+impl Into<u8> for &Exception {
+    fn into(self) -> u8 {
+        self.code()
+    }
+}
+
+
+impl Into<u8> for Exception {
+    fn into(self) -> u8 {
+        self.code()
+    }
+}
+
+
+impl TryFrom<u8> for Exception {
+    type Error = ();
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0x01 => Ok(Self::IllegalCode),
+            0x02 => Ok(Self::IllegalAddress),
+            0x03 => Ok(Self::IllegalDataValue),
+            0x04 => Ok(Self::ServerFailure),
+            0x05 => Ok(Self::Acknowledge),
+            0x06 => Ok(Self::ServerBusy),
+            0x0A => Ok(Self::GatewayUnavail),
+            0x0B => Ok(Self::BadDevice),
+            _    => Err(())
+        }
+    }
+}
